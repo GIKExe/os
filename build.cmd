c09be9247699929ae7a95c506f5b936a15367c26
IETTCQV <i.gortolomei@yandex.ru>
2023-03-28 02:49:47 +0300

    ?

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
-"ASM/fasm.exe" "main.fasm"
+"FASM/fasm.exe" "main.fasm"
 "C:\Program Files (x86)\UltraISO\UltraISO.exe" -file "C:\os\main.bin" -bootfile "C:\os\main.bin" -output "C:\os\main.iso"
 pause
