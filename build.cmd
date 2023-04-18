bd22f9291a97a6935159bdf2a63142560c3dd7f2
Lubyanoy Ivan <pythonblack5000@gmail.com>
2023-04-18 17:32:37 +0300

    Auto ISO

--- build.cmd
+++ build.cmd
@@ -1,8 +1,13 @@
 @echo off
+
+rmdir /s /q out
+mkdir out
+
 cd src
 "..\prog\nasm.exe" main.nasm -f elf32 -o ..\out\main.o
 cd ..
 "prog\tcc.exe" -m32 -c -ffreestanding -nostdlib -nostdinc src\kernel.c -o out\kernel.o
 "prog\ld.exe" --script src\link.ld out\main.o out\kernel.o -o out\kernel.pe
 "prog\objcopy.exe" -O binary out\kernel.pe out\kernel.bin
+"prog\ultraliso.exe" -file out\kernel.bin -bootfile out\kernel.bin -out out\kernel.iso
 pause
